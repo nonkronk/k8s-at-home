1839ac98fa2d6f0aa1ae5bb0a6492aa56ae58dde
Irvan Tristian <irvan@tristian.id>
2022-02-22 09:22:10 +0700

    Added my project

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -109,6 +109,24 @@ resource "cloudflare_record" "hajimari" {
   ttl     = 1
 }
 
+resource "cloudflare_record" "git" {
+  name    = "git"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "cloud" {
+  name    = "cloud"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
+
 resource "cloudflare_record" "echo_server" {
   name    = "echo-server"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
