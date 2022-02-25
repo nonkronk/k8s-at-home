4575361ea7231e8f7ca3aaca8598a56b80bae628
Irvan Tristian <irvan@tristian.id>
2022-02-26 06:06:55 +0700

    initial commit

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -127,15 +127,6 @@ resource "cloudflare_record" "cloud" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "chart" {
-  name    = "chart"
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
-}
-
 resource "cloudflare_record" "echo_server" {
   name    = "echo-server"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
